a3f74c38d3f880cf361d7f88e94d81d07d569655
Gabriel Vilches Alves <vilchesalves@gmail.com>
2018-02-09 15:24:52 +1300

    fix auth redirect and header links

--- blob/resources/views/header.blade.php
+++ blob/resources/views/header.blade.php
@@ -1,18 +1,13 @@
 <nav class="nav blog-nav">
 
     <a class="nav-link active" href="{{ route('index') }}">Home</a>
-    <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
 
-    @if (Route::has('login'))
-        <div class="top-right links">
-            <a class="nav-link" href="#">Input</a>
-            @auth
-                <a href="{{ route('index') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
-            @endauth
-        </div>
-    @endif
+    @auth
+        <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+        <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+    @else
+        <a class="nav-link" href="{{ route('login') }}">Login</a>
+        <a class="nav-link" href="{{ route('register') }}">Register</a>
+    @endauth
 
 </nav>
