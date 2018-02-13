8a2ce940e60c015767705cf056cdbdded7709a9a
Gabriel Vilches Alves <vilchesalves@gmail.com>
2018-02-13 15:43:25 +1300

    controlling posts through a controller

---
+++ blob/app/Http/Controllers/PostsController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    //
+
+    public function create () {
+        return view('post_create');
+    }
+
+    public function store (Request $request) {
+        $collection = Mongo::get()->homestead->posts;
+
+        $date = new DateTime(
+            $request->date . ' ' . $request->time,
+            new DateTimeZone('Pacific/Auckland')
+        );
+    
+        $insertOneResult = $collection->insertOne([
+            'title' => $request->title,
+            'slug' => str_slug($request->title),
+            'body' => $request->body,
+            'date' => new MongoDB\BSON\UTCDateTime($date->getTimestamp() * 1000),
+            'author' => $request->author,
+        ]);
+    
+        $id = $insertOneResult->getInsertedId();
+    
+        return redirect()->route('post.show', [
+            'id' => $id,
+        ]);
+    }
+
+    public function edit ($id) {
+        $collection = Mongo::get()->homestead->posts;
+    
+        $post = $collection->findOne([
+            '_id' => new ObjectID($id),
+        ]);
+    
+        if ($post === null) {
+            abort(404);
+        }
+
+        $post->date = Carbon::instance($post->date->toDateTime())
+            ->setTimeZone('Pacific/Auckland');
+    
+        return view('post_edit', [
+            'post' => $post,
+        ]);
+    }
+
+    public function doEdit ($id, Request $request) {
+        $collection = Mongo::get()->homestead->posts;
+
+        $date = new DateTime(
+            $request->date . ' ' . $request->time,
+            new DateTimeZone('Pacific/Auckland')
+        );
+
+        $updateResult = $collection->updateOne(
+            [
+                '_id' => new ObjectID($id),
+            ],
+            [
+                '$set' => [
+                    'title' => $request->title,
+                    'slug' => str_slug($request->title),
+                    'body' => $request->body,
+                    'date' => new MongoDB\BSON\UTCDateTime($date->getTimestamp() * 1000),
+                    'author' => $request->author,
+                ]
+            ]
+        );
+        
+        return redirect()->route('post.show', [
+            'id' => $id,
+        ]);
+    }
+
+    public function destroy ($id) {
+        return view('post_destroy', [
+            'id' => $id,
+        ]);
+    }
+
+    public function doDestroy ($id) {
+        $collection = Mongo::get()->homestead->posts;
+    
+        $collection->deleteOne([
+            '_id' => new ObjectID($id),
+        ]);
+    
+        return redirect()->route('dashboard');
+    }
+
+    public function show ($id) {
+        $collection = Mongo::get()->homestead->posts;
+    
+        $post = $collection->findOne([
+            '_id' => new ObjectID($id),
+        ]);
+    
+        if ($post === null) {
+            abort(404);
+        }
+    
+        $post->date = Carbon::instance($post->date->toDateTime())
+            ->setTimeZone('Pacific/Auckland');
+            
+        return view('post_show', [
+            'post' => $post,
+        ]);
+    }
+    
+}
--- blob/routes/web.php
+++ blob/routes/web.php
@@ -32,118 +32,14 @@ Route::get('/', function () {
 })->name('index');
 
 Route::middleware(['auth'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
 
-    Route::get('dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    Route::get('post/create', function () {
-        return view('post_create');
-    })->name('post.create');
-    
-    Route::post('post', function (Request $request) {
-        $collection = Mongo::get()->homestead->posts;
-
-        $date = new DateTime(
-            $request->date . ' ' . $request->time,
-            new DateTimeZone('Pacific/Auckland')
-        );
-    
-        $insertOneResult = $collection->insertOne([
-            'title' => $request->title,
-            'slug' => str_slug($request->title),
-            'body' => $request->body,
-            'date' => new MongoDB\BSON\UTCDateTime($date->getTimestamp() * 1000),
-            'author' => $request->author,
-        ]);
-    
-        $id = $insertOneResult->getInsertedId();
-    
-        return redirect()->route('post.show', [
-            'id' => $id,
-        ]);
-    })->name('post.store');
-
-    Route::get('post/{id}/edit', function ($id) {
-        $collection = Mongo::get()->homestead->posts;
-    
-        $post = $collection->findOne([
-            '_id' => new ObjectID($id),
-        ]);
-    
-        if ($post === null) {
-            abort(404);
-        }
-
-        $post->date = Carbon::instance($post->date->toDateTime())
-            ->setTimeZone('Pacific/Auckland');
-    
-        return view('post_edit', [
-            'post' => $post,
-        ]);
-    })->name('post.edit');
-
-    Route::put('post/{id}/edit', function ($id, Request $request) {
-        $collection = Mongo::get()->homestead->posts;
-
-        $date = new DateTime(
-            $request->date . ' ' . $request->time,
-            new DateTimeZone('Pacific/Auckland')
-        );
-
-        $updateResult = $collection->updateOne(
-            [
-                '_id' => new ObjectID($id),
-            ],
-            [
-                '$set' => [
-                    'title' => $request->title,
-                    'slug' => str_slug($request->title),
-                    'body' => $request->body,
-                    'date' => new MongoDB\BSON\UTCDateTime($date->getTimestamp() * 1000),
-                    'author' => $request->author,
-                ]
-            ]
-        );
-        
-        return redirect()->route('post.show', [
-            'id' => $id,
-        ]);
-    })->name('post.edit');
-
-    Route::get('post/{id}/destroy', function ($id) {
-        return view('post_destroy', [
-            'id' => $id,
-        ]);
-    })->name('post.destroy');
-    
-    Route::delete('post/{id}/destroy', function ($id) {
-        $collection = Mongo::get()->homestead->posts;
-    
-        $collection->deleteOne([
-            '_id' => new ObjectID($id),
-        ]);
-    
-        return redirect()->route('dashboard');
-    })->name('post.destroy');
-
+    Route::get('post/create', 'PostsController@create')->name('post.create');
+    Route::post('post', 'PostsController@store')->name('post.store');
+    Route::get('post/{id}/edit', 'PostsController@edit')->name('post.edit');
+    Route::put('post/{id}/edit', 'PostsController@doEdit')->name('post.edit');
+    Route::get('post/{id}/destroy', 'PostsController@destroy')->name('post.destroy');
+    Route::delete('post/{id}/destroy', 'PostsController@doDestroy')->name('post.destroy');
 });
 
-Route::get('post/{id}', function ($id) {
-    $collection = Mongo::get()->homestead->posts;
-
-    $post = $collection->findOne([
-        '_id' => new ObjectID($id),
-    ]);
-
-    if ($post === null) {
-        abort(404);
-    }
-
-    $post->date = Carbon::instance($post->date->toDateTime())
-        ->setTimeZone('Pacific/Auckland');
-        
-    return view('post_show', [
-        'post' => $post,
-    ]);
-})->name('post.show');
+Route::get('post/{id}', 'PostsController@show')->name('post.show');
